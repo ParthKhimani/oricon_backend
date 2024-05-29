7dde27ce3bd8bed51e752b1ed440b4e290fe04d7
ParthKhimani <parthkhimani48@gmail.com>
2024-05-29 22:08:37 +0530

    integrated nodemailer

--- src/Controller/UserAuthenticationController.ts
+++ src/Controller/UserAuthenticationController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import User from "../Model/User";
 import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
 
 const loginUser = async (req: Request, res: Response) => {
-  const { userName, password } = req.body;
+  const { name, userName, password } = req.body;
   const result = await User.findOne({ userName: userName });
   if (!result) {
     res.status(303).json({ msg: "User not found !" });
@@ -12,6 +13,28 @@ const loginUser = async (req: Request, res: Response) => {
     if (passCheck == 0) {
       const token = jwt.sign({ role: "admin" }, "secret-key");
       res.status(200).json({ msg: "User Logged In !", token });
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "parthkhimani48@gmail.com",
+          pass: "bxukavbknqbpcgeu",
+        },
+      });
+
+      const mailOptions = {
+        from: "parthkhimani48@gmail.com",
+        to: "oriconindia1997@gmail.com",
+        subject: "New Login Alert",
+        text: `${name} logged in`,
+      };
+
+      transporter.sendMail(mailOptions, function (err, res) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Email Sent");
+        }
+      });
     } else {
       res.status(400).json({ msg: "Incorrect Password !" });
     }
