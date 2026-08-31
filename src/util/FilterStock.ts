9b0c294e37af28c7403fff43ed2c53baedc9acb0
ParthKhimani <parthkhimani48@gmail.com>
2026-08-31 17:56:10 +0530

    fix: resolved loose packing duplicate entries issue

--- src/util/FilterStock.ts
+++ src/util/FilterStock.ts
@@ -20,10 +20,11 @@ export const FilterStock = (data: IProduct[] | LatestStock[]) => {
   let temp: Stock[] = [];
   data.forEach((item) => {
     if (typeof item.size === "object") {
-      const index = temp?.findLastIndex(
-        (stock) => stock.size === (item.size as ISize)?.size
+      const index = temp?.findIndex(
+        (stock) =>
+          stock.size === (item.size as ISize)?.size && stock.type === item.type,
       );
-      if (index !== -1 && temp[index].type === item.type) {
+      if (index !== -1) {
         temp[index].netWeight += Number(item.netWeight);
         temp[index].count += 1;
       } else
@@ -54,7 +55,7 @@ export const LatestStock = (data: IProduct[]) => {
     ?.filter(
       (item) =>
         moment(item.created_at).utcOffset("+05:30").format("DD/MM/YYYY") ===
-        moment(new Date()).format("DD/MM/YYYY")
+        moment(new Date()).format("DD/MM/YYYY"),
     )
     .map((item) => ({
       netWeight: item?.netWeight,
